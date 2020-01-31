19372472fa6c8cec92eed2a9921d7e8a3808e31d
Luke Beatty <lcharlesb@ip0af5c134.int.uvm.edu>
2020-01-31 09:14:00 -0500

    Finished base and first queries for each function

--- QueriesOutline.py
+++ QueriesOutline.py
@@ -11,6 +11,8 @@
 # searchYear
     # base:
     query = "SELECT Name, Platform, Year FROM tblSales"
+    # by year:
+    query = "SELECT Name, Platform, Year FROM tblSales WHERE Year = ?"
 # searchPlatform
     # base:
     query = "(SELECT Name, Platform FROM tblSales) UNION (SELECT Name, Console FROM tblScores)"
@@ -19,7 +21,7 @@
 # searchRank
     # base:
     query = "SELECT Rank, Name, Platform FROM tblSales"
-    # by number:
+    # by rank:
     query = "SELECT Name FROM tblSales WHERE Rank = ?"
 
 # ===========Ratings========== #
@@ -27,9 +29,15 @@
 # searchTitle
     # base:
     query = "(SELECT Name, Platform FROM tblSales) UNION (SELECT Name, Console FROM tblScores)"
-# searchMetacritic
+    # by title:
+    query = "SELECT Name FROM tblSales WHERE Name = ?"
+# searchMetascore
     # base:
     query = "SELECT Metascore, Name FROM tblScores"
+    # by metascore:
+    query = "SELECT Name, Metascore FROM tblScores WHERE Metascore = ?"
 # searchUserScore
     # base:
     query = "SELECT Userscore, Name FROM tblScores"
+    # by userscore:
+    query = "SELECT Name, Userscore FROM tblScores WHERE Userscore = ?"
